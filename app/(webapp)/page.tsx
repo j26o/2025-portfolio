eba87b96d688640c1e32936948106ea4e62b639d
Ro Baldovino <iam@robaldovino.com>
2025-01-23 22:20:50 +0800

    Add 3D canvas with performance metrics and update styles for the homepage

--- app/(webapp)/page.tsx
+++ app/(webapp)/page.tsx
@@ -1,13 +1,26 @@
 "use client"
 
 import styles from "./page.module.css"
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from "@react-three/drei"
+import { Perf } from 'r3f-perf'
+
 export default function Home() {
   
-
   return (
     <div className={styles.page}>
       <main className={styles.canvas}>
         <h1 className={styles.h1}>Welcome to the portfolio of Roland Baldovino</h1>
+        <Canvas>
+          <Perf position="top-left" />
+          <OrbitControls />
+          <mesh>
+            <boxGeometry args={[2, 2, 2]} />
+            <meshPhongMaterial />
+          </mesh>
+          <ambientLight intensity={0.1} />
+          <directionalLight position={[0, 0, 5]} color="red" />
+        </Canvas>
       </main>
     </div>
   );
